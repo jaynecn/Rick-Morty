c5c5b71830b468fd302074dc4c8353d4a6891993
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-10-10 13:58:38 +0100

    applied dependencies and started App.js

---
+++ jayne-react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
+import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      App
+    </div>
+  );
+}
+
+export default App;
